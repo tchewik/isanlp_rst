5f0c42fdb98038506037e5877ee03e0222298b0a
tchewik <elenachistov@gmail.com>
2020-03-12 11:14:20 +0000

    update segmentation

--- Dockerfile
+++ Dockerfile
@@ -18,8 +18,6 @@ RUN dostoevsky download fasttext-social-network-model
 RUN pip install setuptools==41.0.1 scipy scikit-learn==0.22.1 gensim==3.6.0 smart-open==1.7.0 tensorflow==2.1.0 keras h5py tensorflow-hub pandas nltk imbalanced-learn catboost
 
 RUN pip install allennlp==0.9.0
-RUN python -c "from allennlp.predictors import Predictor; predictor = Predictor.from_path('models/tony_segmentator/model.tar.gz')"
-
 RUN pip install -U git+https://github.com/IINemo/isanlp.git@discourse
 
 RUN python -c "import nltk; nltk.download('stopwords')"
@@ -27,6 +25,9 @@ RUN python -c "import nltk; nltk.download('stopwords')"
 COPY src/isanlp_rst /src/isanlp_rst
 COPY pipeline_object.py /src/isanlp_rst/pipeline_object.py
 COPY models /models
+
+RUN curl -O http://files.deeppavlov.ai/deeppavlov_data/bert/rubert_cased_L-12_H-768_A-12_pt.tar.gz && tar -xzvf rubert_cased_L-12_H-768_A-12_pt.tar.gz
+RUN python -c "from allennlp.predictors import Predictor; predictor = Predictor.from_path('models/tony_segmentator/model.tar.gz')"
 
 ENV PYTHONPATH=/src/isanlp_rst/
 CMD [ "python", "/start.py", "-m", "pipeline_object", "-a", "create_pipeline", "--no_multiprocessing", "True"]
--- src/isanlp_rst/allennlp_segmentator.py
+++ src/isanlp_rst/allennlp_segmentator.py
@@ -1,7 +1,8 @@
 import os
+
 import numpy as np
-from isanlp.annotation_rst import DiscourseUnit
 from allennlp.predictors import Predictor
+from isanlp.annotation_rst import DiscourseUnit
 
 
 class AllenNLPSegmentator:
@@ -11,25 +12,26 @@ class AllenNLPSegmentator:
     LEMMA = 3
     POSTAG = 4
     SYNTAX_DEP_TREE = 5
-    
+
     def __init__(self, model_dir_path):
         self._model_path = os.path.join(model_dir_path, 'tony_segmentator', 'model.tar.gz')
         self.predictor = Predictor.from_path(self._model_path)
         self._separator = 'U-S'
-        
+
     def __call__(self, *args, **kwargs):
-        return self._build_discourse_units(args[self.TEXT], args[self.TOKENS], 
+        return self._build_discourse_units(args[self.TEXT], args[self.TOKENS],
                                            self._predict(args[self.TOKENS], args[self.SENTENCES]))
-        
+
     def _predict(self, tokens, sentences):
         """
         :return: numbers of tokens predicted as EDU left boundaries
         """
         result = []
         for sentence in sentences:
-            result += self.predictor.predict(' '.join([token.text for token in tokens[sentence.begin:sentence.end]])
-                                            )['tags']
-            
+            result += self.predictor.predict(
+                ' '.join([self.prepare_token(token.text) for token in tokens[sentence.begin:sentence.end]])
+            )['tags']
+
         result = np.array(result)
         return np.argwhere(result == self._separator)[:, 0]
 
@@ -40,26 +42,31 @@ class AllenNLPSegmentator:
         :param numbers: positions of tokens predicted as EDU left boundaries (beginners)
         :return: list of DiscourseUnit
         """
-        
+
         edus = []
-    
+
         if numbers.shape[0]:
-            for i in range(0, len(numbers)-1):
+            for i in range(0, len(numbers) - 1):
                 new_edu = DiscourseUnit(i,
                                         start=tokens[numbers[i]].begin,
-                                        end=tokens[numbers[i+1]].begin - 1,
-                                        text=text[tokens[numbers[i]].begin:tokens[numbers[i+1]].begin],
+                                        end=tokens[numbers[i + 1]].begin - 1,
+                                        text=text[tokens[numbers[i]].begin:tokens[numbers[i + 1]].begin],
                                         relation='elementary')
                 edus.append(new_edu)
 
             if numbers.shape[0] == 1:
                 i = -1
-            
-            new_edu = DiscourseUnit(i+1,
-                            start=tokens[numbers[-1]].begin,
-                            end=len(text),
-                            text=text[tokens[numbers[-1]].begin:],
-                            relation='elementary')
+
+            new_edu = DiscourseUnit(i + 1,
+                                    start=tokens[numbers[-1]].begin,
+                                    end=len(text),
+                                    text=text[tokens[numbers[-1]].begin:],
+                                    relation='elementary')
             edus.append(new_edu)
 
         return edus
+
+    def _prepare_token(self, token):
+        for keyword in ['www', 'http']:
+            if keyword in token:
+                return '_html_'
