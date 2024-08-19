bd261d9e9ca1ff6e154494e7836a8e65d41e0d4b
Elena Chistova <elenachistov@gmail.com>
2024-08-19 14:03:27 +0300

    Update parser.py

--- isanlp_rst/parser.py
+++ isanlp_rst/parser.py
@@ -1,20 +1,25 @@
 from .dmrst_parser.predictor import Predictor as PredictorDMRST
 
 class Parser:
+    AVAILABLE_VERSIONS = ('gumrrg', 'rstdt', 'rstreebank')
+
     def __init__(self,
                  model_dir: str = None,
                  hf_model_name: str = 'tchewik/isanlp_rst_v3',
                  hf_model_version: str = None,
                  cuda_device: int = -1):
 
-        if hf_model_version in ('gumrrg', 'rstdt', 'rstreebank'):
-            self.predictor = PredictorDMRST(model_dir=model_dir,
-                                            hf_model_name=hf_model_name,
-                                            hf_model_version=hf_model_version,
-                                            cuda_device=cuda_device)
-
+        if hf_model_version in self.AVAILABLE_VERSIONS:
+            self.predictor = PredictorDMRST(
+                model_dir=model_dir,
+                hf_model_name=hf_model_name,
+                hf_model_version=hf_model_version,
+                cuda_device=cuda_device
+            )
         else:
-            raise NotImplementedError
+            raise NotImplementedError(
+                f"Available options for hf_model_version are: {', '.join(self.AVAILABLE_VERSIONS)}"
+            )
 
-    def __call__(self, text):
+    def __call__(self, text: str):
         return self.predictor.parse_rst(text)
